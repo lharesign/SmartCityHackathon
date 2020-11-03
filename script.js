519e42e61768508900d25cd85cb6fcbed9abbd4b
ramasubram <hariram243>
2020-11-03 17:48:50 +0100

    Added connect to your neighborhood components

---
+++ script.js
@@ -0,0 +1,7 @@
+function addInputContainer() {
+    var x = document.getElementById("input-items");
+    var y = document.getElementById("plus");
+    x.style.display = "block";
+    y.style.display = "none";
+  }
+  
