0501761ec3861e409aa5a6724a6aa22a717fdd97
ramasubram <hariram243>
2020-11-04 13:33:31 +0100

    Help each other update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function addInputContainer() {
     var x = document.getElementById("input-items");
-    var y = document.getElementById("plus");
+    var y = document.getElementById("plussign");
     x.style.display = "flex";
     y.style.display = "none";
   }
